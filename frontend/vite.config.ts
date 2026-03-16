2fd76a8b6b51f7274d13f52c4df2370ae584cbcc
Matheus Soares <37583650+bornerlssgamer@users.noreply.github.com>
2026-03-16 16:59:06 -0300

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,19 +8,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
   },
   server: {
     proxy: {
       "/api": "http://localhost:8000",
-    },
-  },
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === "MODULE_LEVEL_DIRECTIVE") return;
-        warn(warning);
-      },
     },
   },
 });
